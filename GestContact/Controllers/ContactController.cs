6f45311fc7afa27fcb1aaf4dd6b90e8c5c628a81
steveBstorm <steve.lorent@bstorm.be>
2021-08-23 12:13:32 +0200

    exo fini

---
+++ GestContact/Controllers/ContactController.cs
@@ -0,0 +1,66 @@
+﻿using GestContact.Abstractions;
+using GestContact.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestContact.Controllers
+{
+    public class ContactController : Controller
+    {
+        private IContactService _service;
+
+        public ContactController(IContactService service)
+        {
+            _service = service;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_service.GetAll());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contact c)
+        {
+            if(ModelState.IsValid)
+            {
+                _service.AddContact(c);
+                return RedirectToAction("Index");
+            }
+
+            return View(c);
+            
+        }
+
+        public IActionResult Detail(int id)
+        {
+            return View(_service.GetContact(id));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            _service.RemoveContact(id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return View(_service.GetContact(id));
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Contact c)
+        {
+            _service.UpdateContact(c);
+            return RedirectToAction("Index");
+        }
+    }
+}
--- GestContact/Models/Contact.cs
+++ GestContact/Models/Contact.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,16 @@ namespace GestContact.Models
 {
     public class Contact
     {
+        [ScaffoldColumn(false)]
         public int Id { get; set; }
+        [Required]
+        [Display(Name = "Nom de famille")]
         public string LastName { get; set; }
+        [DataType(DataType.Password)]
         public string FirstName { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+   
         public string Email { get; set; }
         public bool IsFavorite { get; set; }
     }
